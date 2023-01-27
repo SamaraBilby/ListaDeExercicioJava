ee82597f35e7452673a76f64edc614db8697df63
SamaraBilby <samarabilby@hotmail.com>
2023-01-27 10:54:39 -0300

    alterando pastas

--- listaExercicioJava/src/listaExercicioJava/Exercicio_1.java
+++ listaExercicioJava/src/listaExercicioJava/Exercicio_1.java
@@ -1,10 +1,29 @@
 package listaExercicioJava;
 
+import java.util.Scanner;
+
 public class Exercicio_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		Scanner sc = new Scanner(System.in);
+		
+		// entrada dos valores
+	
+			int qtdMin, qtdMax, estoqueMedio;
+			
+			System.out.println("Digite a quantidade mínima do estoque: ");
+			qtdMin = sc.nextInt();
+			
+			System.out.println("Digite a quantidade máxima do estoque: ");
+			qtdMax = sc.nextInt();
+			
+			//processamento
+			estoqueMedio = (qtdMin + qtdMax) / 2;
+			
+			//saída
+			System.out.println("O valor do estoque médio é: " + estoqueMedio);
+		sc.close();
 	}
 
 }
