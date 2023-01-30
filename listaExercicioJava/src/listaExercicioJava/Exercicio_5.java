6c59ae045ee4b00f036fb0a905c8da6df360bd9d
SamaraBilby <samarabilby@hotmail.com>
2023-01-30 10:46:31 -0300

    ajuste da numeração dos exercicios

--- listaExercicioJava/src/listaExercicioJava/Exercicio_5.java
+++ listaExercicioJava/src/listaExercicioJava/Exercicio_5.java
@@ -1,5 +1,5 @@
-/**********************************************************************************
- *  Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, 
+/************************************************************************************************************************
+ * 5) Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, 
  *  	utilizando um automóvel que faz 12 Km por litro. Para obter o cálculo, 
  *  	o usuário deve fornecer o tempo gasto na viagem e a velocidade média. Desta forma, 
  *  	será possível obter a distância percorrida com a fórmula DISTANCIA = TEMPO * VELOCIDADE. 
@@ -8,7 +8,7 @@
  *   	O programa deve apresentar os valores da velocidade média, tempo gasto, 
  *   	a distância percorrida e a quantidade de litros utilizada na viagem. Dica: trabalhe com valores reais.
  
- * ********************/
+ * **********************************************************************************************************************/
 
 
 
