f301f1d17caab26cc52f5b8b51280b1edf99e71e
SamaraBilby <samarabilby@hotmail.com>
2023-02-06 11:45:46 -0300

    melhorando a lógica para resolição do problema

--- listaExercicioJava/src/listaExercicioJava/Exercicio_13.java
+++ listaExercicioJava/src/listaExercicioJava/Exercicio_13.java
@@ -1,19 +1,22 @@
-/***********************************************************
- * Dados três valores A, B, C verificar se eles pode ser os 
- * os comprimentos do lados, se forem verificar se compõe um
- * triângulo equilátero, isósceles ou escoleno. 
- * Informar se não compuserem nenhum triângulo.
+/****************************************************************
+ *    13)Dados três valores A, B, C verificar se eles pode ser os 
+ *     os comprimentos do lados, se forem verificar se compõe um
+ *     triângulo equilátero, isósceles ou escoleno. 
+ *     Informar se não compuserem nenhum triângulo.
  *
- * Lógica para resolução
- * Triângulo -  figura geométrica de três lados, em que cada
- * um é menor que a soma dos outros dois;
- * Então: (A <= B + C) && (B <= A + C) && (C <= A + B)
- * Triângulo equilátero - possui três lados iguais;
- * Então : (A == B) && (B == C) 
- * Triângulo isósceles -  possui dois lados iguais;
- * Então: (A == B) || (A == C) || (B == C)
- * Triângulo escaleno - possui todos os lados diferentes
- * Então: (A <> B) && (B <> C) && (A <> C)
+ *      Lógica para resolução:
+ *         Triângulo -  figura geométrica de três lados, em que cada
+ *           um é menor que a soma dos outros dois;
+ *           Então: (A <= B + C) && (B <= A + C) && (C <= A + B);
+ * 
+ *          Triângulo equilátero - possui três lados iguais;
+ *            Então : (A == B) && (B == C) ;
+ *          
+ *          Triângulo isósceles -  possui dois lados iguais;
+ *            Então: (A == B) || (A == C) || (B == C);
+ * 
+ *          Triângulo escaleno - possui todos os lados diferentes
+ *            Então: (A <> B) && (B <> C) && (A <> C);
  ***********************************************************/
 
 
