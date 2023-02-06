73d26a9deac39ab1ac6d662a4d622a1eab7f5f2c
SamaraBilby <samarabilby@hotmail.com>
2023-02-06 11:38:41 -0300

    Exercício_13

--- listaExercicioJava/src/listaExercicioJava/Exercicio_12.java
+++ listaExercicioJava/src/listaExercicioJava/Exercicio_12.java
@@ -10,6 +10,9 @@ package listaExercicioJava;
 import java.util.Scanner;
 
 public class Exercicio_12 {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
             int numero, modulo;
