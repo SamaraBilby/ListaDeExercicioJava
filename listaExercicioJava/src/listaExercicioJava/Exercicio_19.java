29d4d9a4f85d555b15a9f9cddcd6aeac91c679ff
SamaraBilby <samarabilby@hotmail.com>
2023-02-06 17:17:53 -0300

    add título do exercício_19

---
+++ listaExercicioJava/src/listaExercicioJava/Exercicio_19.java
@@ -0,0 +1,20 @@
+/*****************************************************************
+ *  19) Escreva um programa que leia o código de um determinado 
+ *      produto e mostre a sua classificação.
+ *      
+ *      cod                     Classificação
+ *      1                       Alimento não-perecível
+ *      2                       Alimento perecível
+ *      5 ou 6                  Vestuário
+ *      7                       Higiene pessoal
+ *      8 até 15                Limpeza e utensílios domésticos
+ *      Qualquer outro código   Inválido
+ *****************************************************************/
+
+package listaExercicioJava;
+
+public class Exercicio_19 {
+    public static void main(String[] args){
+
+    }
+}
